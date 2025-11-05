fb755b36723b0a7c1de058e9be89a66fa37e17f5
honoursbhaduria <honoursbhadauria@gmail.com>
2025-11-05 17:16:35 -0600

    vapi integration

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,9 +5,9 @@ import {isAuthenticated} from "@/lib/actions/auth.action";
 import {redirect} from "next/navigation";
 
 const RootLayout = async ({ children }: { children: ReactNode }) => {
-    const isUserAuthenticated = await isAuthenticated();
+    // const isUserAuthenticated = await isAuthenticated();
 
-    if(!isUserAuthenticated) redirect('/sign-in');
+    // if(!isUserAuthenticated) redirect('/sign-in');
 
     return (
         <div className="root-layout">
